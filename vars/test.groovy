55b212faeaae1c603ef91307aff4d86b5d0d8d49
Eliana Ferioli <elianaferioli@gmail.com>
2019-07-31 14:11:15 +0200

    Update test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,11 @@
+def testInflux(){
+    step([$class: 'InfluxDbPublisher',
+                  customData: null,
+                  customDataMap: null,
+                  customPrefix: null,
+                  //jenkinsEnvParameterTag: 'KEY=' + env.BUILD_NUMBER,
+                  jenkinsEnvParameterField: 'environment=develop',
+                  measurementName: 'prueba_jenkins',// OPTIONAL, custom fields
+                  target: 'prueba_jenkins'])
+      } 
+}
