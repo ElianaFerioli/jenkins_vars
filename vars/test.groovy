d6de088016ec25384e9d549f5c05b60ec53ff6e2
Eliana Ferioli <elianaferioli@gmail.com>
2019-08-01 14:05:50 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,6 @@
 def call(String environment_name, String squad_name){
     step([$class: 'InfluxDbPublisher',
-                  jenkinsEnvParameterField: 'environment_name=' + "${environment_name}" + ",test=test",
+                  jenkinsEnvParameterField: 'environment_name=' + "${environment_name}" + ',test=test',
                   measurementName: 'prueba_jenkins',// OPTIONAL, custom fields
                   target: 'prueba_jenkins'])
 }
