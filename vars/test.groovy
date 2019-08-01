0339cc4900777b6056eb51dc58cac60efd8dcb70
Eliana Ferioli <elianaferioli@gmail.com>
2019-08-01 09:21:43 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,5 @@
 def call(String environment){
     step([$class: 'InfluxDbPublisher',
-                  customData: null,
-                  customDataMap: null,
-                  customPrefix: null,
-                  //jenkinsEnvParameterTag: 'KEY=' + env.BUILD_NUMBER,
                   jenkinsEnvParameterField: 'environment=' + "${environment}",
                   measurementName: 'prueba_jenkins',// OPTIONAL, custom fields
                   target: 'prueba_jenkins'])
