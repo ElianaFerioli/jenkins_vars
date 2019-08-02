bdc8fc64063ec0110e01c7ae355159507a5067ed
Eliana Ferioli <elianaferioli@gmail.com>
2019-08-02 08:24:20 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,10 @@
 def call(String environment_name, String squad_name){
+    def myDataMap = [:]
+    myDataMap['myKey'] = 'myValue'
     step([$class: 'InfluxDbPublisher',
-                  jenkinsEnvParameterField: 'environment_name=' + "${environment_name}" + " " + "test=test",
+                  //jenkinsEnvParameterField: 'environment_name=' + "${environment_name}" + " " + "test=test",
                   //jenkinsEnvParameterTag: 'squad_name=' + "${squad_name}" + ",TEST=TEST",
+                  customData: myDataMap,
                   measurementName: 'prueba_jenkins',// OPTIONAL, custom fields
                   target: 'prueba_jenkins'])
 }
