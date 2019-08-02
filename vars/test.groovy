655cf902bba7b99e2483206000b9667323267908
Eliana Ferioli <elianaferioli@gmail.com>
2019-08-02 09:18:11 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,21 @@
 def call(String environment_name, String squad_name){
-    def myDataMap = [:]
-    myDataMap['environment_name'] = "${environment_name}"
-    myDataMap['squad_name'] = "${squad_name}"
-    myDataMap['build_result'] =  "${currentBuild.currentResult}"
+    def dataMap = [:]
+    dataMap['environment_name'] = "${environment_name}"
+    dataMap['squad_name'] = "${squad_name}"
+    dataMap['build_result'] =  "${currentBuild.currentResult}"
+    if("${currentBuild.currentResult}"=='SUCCESS'){
+        dataMap['build_result_ordinal'] =  0
+    }
+    if("${currentBuild.currentResult}"=='FAILURE'){
+        dataMap['build_result_ordinal'] =  2
+    }
+    if("${currentBuild.currentResult}"=='UNSTABLE'){
+        dataMap['build_result_ordinal'] =  0
+    }
     step([$class: 'InfluxDbPublisher',
                   //jenkinsEnvParameterField: 'environment_name=' + "${environment_name}" + " " + "test=test",
                   //jenkinsEnvParameterTag: 'squad_name=' + "${squad_name}" + ",TEST=TEST",
-                  customData: myDataMap,
+                  customData: dataMap,
                   measurementName: 'prueba_jenkins',// OPTIONAL, custom fields
                   target: 'prueba_jenkins'])
 }
